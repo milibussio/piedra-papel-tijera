08489c9f51596e87fb680da41feb195a6c12632a
Milagros Bussio <mdbussio@uc.cl>
2023-01-30 17:55:52 -0300

    Se agrega funcionalidad a interfaz
    Se agrega posiblidad de jugar con UI, implica agregar etiquetas en HTLM y otros cambios menores y cambiar js. Se ajustan aspectos del CSS también.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function getComputerChoice() {
-    let elecciones =["piedra", "papel", "tijera"];
-    return elecciones[Math.floor(Math.random()*3)]
-}
+// Acá tenemos las funciones para el juego de priedra papel o tijera
+
+/* Esta función era para la consola, ya no la usamos.
 
 function getPlayerChoice(){
     let elecciones =["piedra", "papel", "tijera"];
@@ -12,32 +11,10 @@ function getPlayerChoice(){
     } else {
         return getPlayerChoice()
     }
-
 }
+*/
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return `Es un empate, ambos eligieron ${computerSelection}.`;
-    } else if (playerSelection == "piedra") {
-        if (computerSelection == "papel") {
-            return "¡Perdiste! Piedra pierde contra papel."        
-        } else {
-            return "¡Ganaste! Piedra le gana a tijera."
-        }
-    } else if (playerSelection == "papel") {
-        if (computerSelection == "tijera") {
-            return "¡Perdiste! Papel pierde contra tijera."        
-        } else {
-            return "¡Ganaste! Papel le gana a piedra."
-        }
-    } else {
-        if (computerSelection == "piedra") {
-            return "¡Perdiste! Tijera pierde contra piedra."        
-        } else {
-            return "¡Ganaste! Tijera le gana a papel."
-        }
-    }
-}
+/* esta también quedó fuera, no la vamos a usar
 
 function playMatch(){
     let playerScore = 0;
@@ -67,5 +44,114 @@ function playMatch(){
         console.log("Felicidades!! Ganaste la partida!!")
     } else {console.log("Mala suerte, perdiste la partida.")}
 }
+*/
 
-playMatch()
+function getComputerChoice() {
+    let elecciones =["piedra", "papel", "tijera"];
+    return elecciones[Math.floor(Math.random()*3)];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let topScore = 0;
+const ptjeJugador = document.querySelector('#ptjeJugador');
+const ptjePC = document.querySelector('#ptjePC');
+const frase = document.querySelector('#frase');
+
+function playRound(playerSelection, computerSelection){
+
+    if (playerSelection == computerSelection){
+        frase.textContent = '¡Empataron!';
+    } else if (playerSelection == "piedra") {
+        if (computerSelection == "papel") {
+            frase.textContent = '¡Perdiste!'
+            computerScore++;
+            ptjePC.textContent = computerScore;                    
+        } else {
+            frase.textContent = '¡¡Ganaste!!';
+            playerScore++;
+            ptjeJugador.textContent = playerScore; 
+        }
+    } else if (playerSelection == "papel") {
+        if (computerSelection == "tijera") {
+            frase.textContent = '¡Perdiste!';
+            computerScore++;
+            ptjePC.textContent = computerScore;     
+        } else {
+            frase.textContent = '¡¡Ganaste!!';
+            playerScore++;
+            ptjeJugador.textContent = playerScore; 
+        }
+    } else {
+        if (computerSelection == "piedra") {
+            frase.textContent = '¡Perdiste!';
+            computerScore++;
+            ptjePC.textContent = computerScore;         
+        } else {
+            frase.textContent = '¡¡Ganaste!!';
+            playerScore++;
+            ptjeJugador.textContent = playerScore; 
+        }
+    }
+
+    if (playerScore == 5 || computerScore == 5){
+        partidaTerminada()
+    }
+}
+
+function partidaTerminada() {
+    if (playerScore > computerScore){
+    frase.textContent = `Ganaste ${playerScore} a ${computerScore}. Haz click en cualquier opción para volver a jugar.`;
+    } else {frase.textContent = `Ganaste ${computerScore} a ${playerScore}. Haz click en cualquier opción para volver a jugar.`;
+    }
+    ptjeJugador.textContent = 0;
+    ptjePC.textContent = 0; 
+    playerScore = 0;
+    computerScore = 0;
+    topScore = 0; 
+    const suEleccion =document.querySelector('#suEleccion');
+    suEleccion.src=("images/gris.jpeg");
+    const tuEleccion =document.querySelector('#tuEleccion');
+    tuEleccion.src=("images/gris.jpeg");
+}
+
+// Aca vamos a empezar con la manipulación del DOM
+
+// Para cambiar la imagen del PC
+    
+function ponerEleccionComputador(){
+    const suEleccion =document.querySelector('#suEleccion');
+    let computerChoice = getComputerChoice();
+    suEleccion.src=("images/"+computerChoice+".png");
+    return computerChoice;
+}
+
+// Para cambiar la del jugador e iniciar el juego
+
+function ponerPiedraJugador(){
+    const tuEleccion =document.querySelector('#tuEleccion');
+    tuEleccion.src="images/piedra.png";
+    playRound("piedra", ponerEleccionComputador());
+}
+
+function ponerPapelJugador(){
+    const tuEleccion =document.querySelector('#tuEleccion');
+    tuEleccion.src="images/papel.png";
+    playRound("papel", ponerEleccionComputador())
+}
+
+function ponerTijeraJugador(){
+    const tuEleccion =document.querySelector('#tuEleccion');
+    tuEleccion.src="images/tijera.png";
+    playRound("tijera", ponerEleccionComputador())
+}
+
+const piedra = document.querySelector('#piedra');
+piedra.addEventListener('click', ponerPiedraJugador);
+
+const papel = document.querySelector('#papel');
+papel.addEventListener('click', ponerPapelJugador);
+
+const tijera = document.querySelector('#tijera');
+tijera.addEventListener('click', ponerTijeraJugador);
+
